2b2732ca400b44da398d73ec784fd4dafe3b5e91
황지민 <80872558+krapoi@users.noreply.github.com>
2022-09-26 10:49:18 +0900

    add WritePeriodActivity TextChangeListener

--- app/src/main/java/com/example/projectsilsoup/view/activity/WritePeriodActivity.kt
+++ app/src/main/java/com/example/projectsilsoup/view/activity/WritePeriodActivity.kt
@@ -2,17 +2,21 @@ package com.example.projectsilsoup.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.core.widget.addTextChangedListener
 import com.example.projectsilsoup.R
+import com.example.projectsilsoup.category.Category
 import com.example.projectsilsoup.databinding.ActivityWriteBinding
 import com.example.projectsilsoup.listener.ItemSelectedListenerDay
 import com.example.projectsilsoup.listener.ItemSelectedListenerMonth
+import com.example.projectsilsoup.network.room.entity.ScheduleEntity
+import com.example.projectsilsoup.vm.activity.WriteScheduleModel
 
 class WritePeriodActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityWriteBinding.inflate(layoutInflater) }
+
+    private val model = WriteScheduleModel.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +39,26 @@ class WritePeriodActivity : AppCompatActivity() {
         binding.finishMonth.onItemSelectedListener = ItemSelectedListenerMonth()
         binding.startDay.onItemSelectedListener = ItemSelectedListenerDay()
         binding.finishDay.onItemSelectedListener = ItemSelectedListenerDay()
+
+
+        binding.write.addTextChangedListener {
+            if (binding.periodTitle.text.toString() != "" && binding.periodContent.text.toString() != "") {
+                binding.write.setTextColor(getColor(R.color.black))
+                binding.write.setOnClickListener {
+
+                    val entity = ScheduleEntity(binding.periodTitle.text.toString(), binding.periodContent.text.toString(), Category.PERIOD.toString(), false)
+                    entity.error = binding.periodWarningContent.text.toString()
+                    entity.mapping = binding.periodMapping.text.toString()
+                    val date = "${binding.startMonth.selectedItem}/${binding.startDay.selectedItem} ~ ${binding.finishMonth.selectedItem}/${binding.finishDay.selectedItem}"
+                    entity.date = date
+
+                    model.insertAndUpdate(entity)
+                }
+            }else {
+                binding.write.setTextColor(getColor(R.color.gray))
+                binding.write.setOnClickListener {}
+            }
+        }
 
     }
 }
