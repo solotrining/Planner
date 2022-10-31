f962fda8387facdc1bdae95051c56e4da70811ea
황지민 <80872558+krapoi@users.noreply.github.com>
2022-10-31 12:22:31 +0900

    ADD : widget

--- app/src/main/java/com/example/projectsilsoup/view/fragment/MainFragment.kt
+++ app/src/main/java/com/example/projectsilsoup/view/fragment/MainFragment.kt
@@ -6,14 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.projectsilsoup.R
+import com.example.projectsilsoup.databinding.FragmentMainBinding
+import com.example.projectsilsoup.network.room.entity.ScheduleEntity
+import com.example.projectsilsoup.vm.fragment.ScheduleModel
+import java.util.*
 
 class MainFragment : Fragment() {
+
+    private val model = ScheduleModel.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        val cal = Calendar.getInstance()
+
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+        val nowPlan : List<ScheduleEntity> = model.getPlanByDate("${year}-${month}-${day}")
+
+        if (nowPlan.isNotEmpty()) binding.todayPlan.text = nowPlan[0].title
+
+        return binding.root
     }
 
 }
