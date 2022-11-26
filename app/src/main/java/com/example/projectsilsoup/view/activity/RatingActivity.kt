866cc51749dd34928391b3b3c4a0e1a6169bb7ed
황지민 <80872558+krapoi@users.noreply.github.com>
2022-11-27 05:46:55 +0900

    ADD : alarm

---
+++ app/src/main/java/com/example/projectsilsoup/view/activity/RatingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.projectsilsoup.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectsilsoup.R
+
+class RatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating)
+
+    }
+}
